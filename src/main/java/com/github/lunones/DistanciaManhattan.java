3d37bd88dffc6a464702a7e080ffb6c458634552
Lunones <jlunjim0512@g.educaand.es>
2024-06-24 12:59:35 +0200

    Comentario 1

--- src/main/java/com/github/lunones/DistanciaManhattan.java
+++ src/main/java/com/github/lunones/DistanciaManhattan.java
@@ -2,9 +2,16 @@ package com.github.lunones;
 
 /**
  * Clase que calcula la distancia Manhattan
+ * @author Lunones
  */
 public class DistanciaManhattan extends Distancia {
 
+    /**
+     * Este metodo calcula la distancia recorrida entre dos puntos
+     * @param origen es el punto de partida
+     * @param destino es el punto de destino
+     * @return
+     */
     @Override
     double medir(Punto origen, Punto destino) {
         return Math.abs(destino.getX() - origen.getX()) + Math.abs(destino.getY() - origen.getY());
--- src/main/java/com/github/lunones/TransportistaFede.java
+++ src/main/java/com/github/lunones/TransportistaFede.java
@@ -1,5 +1,9 @@
 package com.github.lunones;
 
+/**
+ * Esta clase implementa el transportista Fede
+ * @author Lunones
+ */
 public class TransportistaFede extends Transportista {
     TransportistaFede() {
         this.nombre = "Fede";
@@ -7,6 +11,14 @@ public class TransportistaFede extends Transportista {
         this.distancia = new DistanciaManhattan();
     }
 
+    /**
+     * Esta clase calcula el presupuesto del envio
+     * @param origen es el punto de partida del envio
+     * @param destino y este es el punto de destino del envio
+     * @param peso el peso que se transporta
+     * @return devuelve el ingreso a pagar por el envio
+     * @throws EnvioNoDisponibleException lanza una excepcion cuando la distancia es de mas de 100km
+     */
     @Override
     public double presupuestar(Punto origen, Punto destino, double peso) throws EnvioNoDisponibleException {
         double distanciaEntrePuntos = distancia.medir(origen, destino);
