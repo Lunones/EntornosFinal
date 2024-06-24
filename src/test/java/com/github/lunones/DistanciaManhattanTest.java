ba272858dc4ec7fcc17a0ba1ba37aa1fdb8166da
Lunones <jlunjim0512@g.educaand.es>
2024-06-24 10:52:58 +0200

    Clases del alumno

--- src/test/java/com/github/lunones/DistanciaManhattanTest.java
+++ src/test/java/com/github/lunones/DistanciaManhattanTest.java
@@ -1,7 +1,45 @@
 package com.github.lunones;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DistanciaManhattanTest {
+    static DistanciaManhattan distancia;
 
+    @BeforeAll
+    public static void setUpClass(){
+        distancia = new DistanciaManhattan();
+    }
+    @Test
+    void distancia_1_2_al_3_4() {
+        Punto origen = new Punto(1,2);
+        Punto destino = new Punto(3,4);
+        assertEquals(distancia.medir(origen,destino), 4);
+    }
+
+
+    @Test
+    void distancia_1_1_al_3_3() {
+        Punto origen = new Punto(1,1);
+        Punto destino = new Punto(3,3);
+        assertEquals(distancia.medir(origen,destino), 4);
+    }
+
+
+    @Test
+    void distancia_menos_2_menos_1_al_1_2() {
+        Punto origen = new Punto(-2,-1);
+        Punto destino = new Punto(1,2);
+        assertEquals(distancia.medir(origen,destino), 6);
+    }
+
+
+    @Test
+    void distancia_menos_1_1_al_3_1() {
+        Punto origen = new Punto(-1,1);
+        Punto destino = new Punto(3,1);
+        assertEquals(distancia.medir(origen,destino), 4);
+    }
 }
--- src/test/java/com/github/lunones/TransportistaFedeTest.java
+++ src/test/java/com/github/lunones/TransportistaFedeTest.java
@@ -1,7 +1,55 @@
 package com.github.lunones;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransportistaFedeTest {
+    static TransportistaFede transportista;
 
+    @BeforeAll
+    public static void setUpClass(){
+        transportista = new TransportistaFede();
+    }
+    @Test
+    void coste_1_2_al_3_4_peso_5() throws Transportista.EnvioNoDisponibleException {
+        Punto origen = new Punto(1,2);
+        Punto destino = new Punto(3,4);
+        double peso = 5;
+        assertEquals(transportista.presupuestar(origen,destino,peso), 5);
+    }
+
+    @Test
+    void coste_0_0_al_0_1_peso_5() throws Transportista.EnvioNoDisponibleException {
+        Punto origen = new Punto(0,0);
+        Punto destino = new Punto(0,1);
+        double peso = 5;
+        assertEquals(transportista.presupuestar(origen,destino,peso), 5);
+    }
+
+
+    @Test
+    void coste_0_0_al_5_15_peso_2() throws Transportista.EnvioNoDisponibleException {
+        Punto origen = new Punto(0,0);
+        Punto destino = new Punto(5,15);
+        double peso = 2;
+        assertEquals(transportista.presupuestar(origen,destino,peso), 20);
+    }
+
+
+    @Test
+    void coste_0_0_al_100_100_peso_5() throws Transportista.EnvioNoDisponibleException {
+        Punto origen = new Punto(0,0);
+        Punto destino = new Punto(100,100);
+        double peso = 58;
+        //assertEquals(transportista.presupuestar(origen,destino,peso), 5);
+
+        Exception thrown = assertThrows(Transportista.EnvioNoDisponibleException.class, () -> {
+            transportista.presupuestar(origen,destino,peso);
+        });
+
+        assertEquals("La distancia de envío no está soportada", thrown.getMessage());
+
+    }
 }
